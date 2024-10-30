65672275978c898bd1a6d266adf5c60a6ea290c5
Carlos Santos <4a.santos@gmail.com>
2024-10-30 15:47:45 +0100

    backend: Update appearance controller

--- backend/src/controllers/global-preferences/appearance-preferences.controller.ts
+++ backend/src/controllers/global-preferences/appearance-preferences.controller.ts
@@ -1,36 +1,13 @@
 import { Request, Response } from 'express';
 
 export const updateAppearancePreferences = async (req: Request, res: Response) => {
-	const { appearancePreferences } = req.body;
-
-	try {
-		// const preferences = await AppearancePreferencesModel.findOne();
-
-		// if (preferences) {
-		// 	preferences.theme = appearancePreferences.theme;
-		// 	await preferences.save();
-		// 	return res.status(200).json({ message: 'Appearance preferences updated successfully.' });
-		// } else {
-		// 	await AppearancePreferencesModel.create({ appearancePreferences });
-			return res.status(201).json({ message: 'Appearance preferences created successfully.' });
-		// }
-	} catch (error) {
-		console.error('Error saving appearance preferences:', error);
-		return res.status(500).json({ message: 'Error saving appearance preferences', error });
-	}
+	return res
+		.status(402)
+		.json({ message: 'Storing appearance preference is a PRO feature. Please, Updrade to OpenVidu PRO' });
 };
 
 export const getAppearancePreferences = async (req: Request, res: Response) => {
-	try {
-		// const preferences = await AppearancePreferencesModel.findOne();
-
-		// if (preferences) {
-		// 	return res.status(200).json(preferences);
-		// } else {
-			return res.status(404).json({ message: 'Appearance preferences not found' });
-		// }
-	} catch (error) {
-		console.error('Error fetching appearance preferences:', error);
-		return res.status(500).json({ message: 'Error fetching appearance preferences', error });
-	}
+	return res
+		.status(402)
+		.json({ message: 'Getting appearance preference is a PRO feature. Please, Updrade to OpenVidu PRO' });
 };
