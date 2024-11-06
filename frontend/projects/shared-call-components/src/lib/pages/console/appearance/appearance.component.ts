2c8f88ba806c629f214fadf04ebaae1c4a2f1c27
Carlos Santos <4a.santos@gmail.com>
2024-11-06 17:40:01 +0100

    frontend: Clean appearance component

--- frontend/projects/shared-call-components/src/lib/pages/console/appearance/appearance.component.ts
+++ frontend/projects/shared-call-components/src/lib/pages/console/appearance/appearance.component.ts
@@ -1,20 +1,13 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
-import { DynamicGridComponent, LogoCardComponent, ProFeatureCardComponent } from '../../../components';
+import { Component } from '@angular/core';
+import { DynamicGridComponent, ProFeatureCardComponent } from '../../../components';
 
 @Component({
 	selector: 'ov-appearance',
 	standalone: true,
-	imports: [DynamicGridComponent, LogoCardComponent, ProFeatureCardComponent],
+	imports: [DynamicGridComponent, ProFeatureCardComponent],
 	templateUrl: './appearance.component.html',
 	styleUrl: './appearance.component.scss'
 })
 export class AppearanceComponent {
-	constructor(protected cdr: ChangeDetectorRef) {}
-
-	// async ngOnInit() {
-	// 	try {
-	// 		await this.loadAppearancePreferences();
-	// 		this.cdr.detectChanges();
-	// 	} catch (error) {}
-	// }
+	constructor() {}
 }
